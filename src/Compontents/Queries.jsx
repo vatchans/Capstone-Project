386e5781b6d16fb46de2322399e55b862f44bbf4
vatchans <112924038+vatchans@users.noreply.github.com>
2023-04-17 11:49:51 +0530

    Update Queries.jsx

--- src/Compontents/Queries.jsx
+++ src/Compontents/Queries.jsx
@@ -39,7 +39,7 @@ function Queries() {
 
                     </div>
                 </div>
-                <TableContainer sx={{ maxHeight: 440 }} className='Table' >
+                <TableContainer sx={{ maxHeight: 440 }} className='Table mt-5' >
         <Table stickyHeader aria-label="sticky table" >
           <TableHead>
             <TableRow>
