99f9a536fa263473dc356238d804cf09005b7bb5
vatchans <112924038+vatchans@users.noreply.github.com>
2023-04-17 11:48:08 +0530

    Update AdminDashboard.jsx

--- src/AdminCompontents/AdminDashboard.jsx
+++ src/AdminCompontents/AdminDashboard.jsx
@@ -151,7 +151,7 @@ function AdminDashboard() {
 
         </div>
       </div>
-      <TableContainer sx={{ maxHeight: 440 }} className='Table' >
+      <TableContainer sx={{ maxHeight: 440 }} className='Table mt-5' >
         <Table stickyHeader aria-label="sticky table" >
           <TableHead>
             <TableRow>
