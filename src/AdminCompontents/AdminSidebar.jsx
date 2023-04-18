e7f67b0410b6d73a9e6c1bafd37c6c2e892767c1
vatchans <112924038+vatchans@users.noreply.github.com>
2023-04-18 14:48:04 +0530

    Update AdminSidebar.jsx

--- src/AdminCompontents/AdminSidebar.jsx
+++ src/AdminCompontents/AdminSidebar.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { useState,useContext } from 'react'
 import { Avatar } from '@mui/material'
+import GroupsIcon from '@mui/icons-material/Groups';
+import AddBoxIcon from '@mui/icons-material/AddBox';
+import jwtDecode from 'jwt-decode';
 function AdminSidebar() {
   let [Showdropdown,hide]=useState(false)
    let Show_dropdown=()=>{
       hide(!Showdropdown)
    }
+   let token = localStorage.getItem('Admin')
+   let decoded = jwtDecode(token)
   return <>
   <nav className="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
  <div className="px-3 py-3 lg:px-5 lg:pl-3">
@@ -18,10 +23,10 @@ function AdminSidebar() {
               <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
            </svg>
         </button>
-       <a href="https://flowbite.com" className="flex ml-2 md:mr-24 ">
+       <div className="flex ml-2 md:mr-24 ">
          <img src="Milky_Way_farm-removebg-preview (1).png" className="h-10 mr-3" alt="Logo" />
          <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Admin portal</span>
-       </a>
+       </div>
      </div>
      <div className="flex items-center">
          <div className="flex items-center ml-3">
@@ -36,15 +41,15 @@ function AdminSidebar() {
               <div className="z-50  my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600 drop-down-item">
              <div className="px-4 py-3" role="none">
                <p className="text-sm text-gray-900 dark:text-white" role="none">
-                 Neil Sims
+                 {decoded.Username}
                </p>
                <p className="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
-                 neil.sims@flowbite.com
+                 {decoded.Email}
                </p>
              </div>
              <ul className="py-1" role="none">
                <li>
-                 <Link to='/CRM' style={{textDecoration:"none"}} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Dashboard</Link>
+                 <Link to='/AdminDashboard' style={{textDecoration:"none"}} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Dashboard</Link>
                </li>
                <li>
                  <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Settings</a>
@@ -81,18 +86,18 @@ function AdminSidebar() {
            </Link>
         </li>
         <li>
-           <Link to='/Resolved'style={{textDecoration:"none"}} className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-              <svg aria-hidden="true" className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z"></path><path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z"></path></svg>
+           <Link to='/Customer_Executives'style={{textDecoration:"none"}} className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+              <GroupsIcon className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"></GroupsIcon>
               <span className="flex-1 ml-3 whitespace-nowrap">Customer Executives</span>
            </Link>
         </li>
         <li>
-           <Link to="/Customers" style={{textDecoration:"none"}} className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+           <Link to="/All_Customers" style={{textDecoration:"none"}} className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
               <svg aria-hidden="true" className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
               <span className="flex-1 ml-3 whitespace-nowrap">Customers</span>
            </Link>
            <Link to="/Add_Product" style={{textDecoration:"none"}} className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-              <svg aria-hidden="true" className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
+              <AddBoxIcon className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
               <span className="flex-1 ml-3 whitespace-nowrap">Add a Product</span>
            </Link>
         </li>
